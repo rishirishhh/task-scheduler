59078e7a0ea42e6a68ce8d0fd0300f5e25add65f
rishi-19-04 <rishikavya19@gmail.com>
2024-06-02 01:50:17 +0530

    added worker code

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module task-scheduler
 go 1.22.3
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.18.3
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.1
 )
 
 require (
@@ -17,6 +20,9 @@ require (
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 )
--- pkg/grpcapi/build.sh
+++ pkg/grpcapi/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Ensure the script is executed in bash
-if [ -z "$BASH_VERSION" ]; then
-  echo "This script must be run with bash, not sh."
-  exit 1
-fi
-
 # Get the directory where the script resides
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -13,9 +7,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROTO_FILE="$SCRIPT_DIR/api.proto"
 
 # Check if protoc is installed and in the PATH
-if ! command -v protoc &> /dev/null
-then
+if ! command -v protoc &> /dev/null; then
     echo "protoc could not be found. Please install it and ensure it's in your PATH."
+    exit 1
+fi
+
+# Check if protoc-gen-go is installed and in the PATH
+if ! command -v protoc-gen-go &> /dev/null; then
+    echo "protoc-gen-go could not be found. Please install it and ensure it's in your PATH."
+    exit 1
+fi
+
+# Check if protoc-gen-go-grpc is installed and in the PATH
+if ! command -v protoc-gen-go-grpc &> /dev/null; then
+    echo "protoc-gen-go-grpc could not be found. Please install it and ensure it's in your PATH."
     exit 1
 fi
 
@@ -24,7 +29,3 @@ protoc --proto_path="$SCRIPT_DIR" \
   --go_out="$SCRIPT_DIR" --go_opt=paths=source_relative \
   --go-grpc_out="$SCRIPT_DIR" --go-grpc_opt=paths=source_relative \
   "$(basename "$PROTO_FILE")"
-
-# Uncomment the following lines if needed
-# protoc --go_out=. --go_opt=paths=source_relative \
-#   --go-grpc_out=. --go-grpc_opt=paths=source_relative api.proto
